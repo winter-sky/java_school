fcc7298c38b2e8abdccc74f84f6e2fec311efd28
winter-sky <dalakovaas@gmail.com>
2018-11-07 13:06:11 +0300

    Minot test addition

--- src/test/java/mainpackage/controller/CustomersControllerTest.java
+++ src/test/java/mainpackage/controller/CustomersControllerTest.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class CustomersControllerTest extends ControllersTestBase {
     @Test
     public void testCustomer() throws Exception  {
-        mockMvc.perform(get("/listcustomer")).andDo(print())
+        mockMvc.perform(get("/listcustomer")).andExpect(status().isOk()).andDo(print())
                 .andExpect(view().name("listcustomers"));
     }
 }
